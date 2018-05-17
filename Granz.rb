4ac3c7e2a35ac61c7d443cb5db67c4028ae4dc4c
GeopJr <geopjr@outlook.com>
2018-05-17 22:48:06 +0300

    Embeds on avatar command and better size

--- Granz.rb
+++ Granz.rb
@@ -97,13 +97,19 @@ end
 $bot.command :avatar, min_args: 1, max_args: 1 do |event, user|
 user = user[2..-2]
 	begin
-		mentioned_user = $bot.user(user);
-		event.respond "#{mentioned_user.mention}'s avatar is #{mentioned_user.avatar_url}"
+			mentioned_user = $bot.user(user);
+			event.channel.send_embed do |embed|
+			embed.colour = 0xffff00
+			embed.image = Discordrb::Webhooks::EmbedImage.new(url: "#{mentioned_user.avatar_url}?size=1024")
+end
 	rescue
 		begin
 			user = user[1..-1]
 			mentioned_user = $bot.user(user);
-			event.respond "#{mentioned_user.mention}'s avatar is #{mentioned_user.avatar_url}"
+			event.channel.send_embed do |embed|
+			embed.colour = 0xffff00
+			embed.image = Discordrb::Webhooks::EmbedImage.new(url: "#{mentioned_user.avatar_url}?size=1024")
+			end
 		end
 	end
 end
