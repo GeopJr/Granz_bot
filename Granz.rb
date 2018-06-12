6016df4906e116782e5b46c88a526d4ccb2ce442
GeopJr <geopjr@outlook.com>
2018-06-12 23:35:26 +0300

    Added rescues

--- Granz.rb
+++ Granz.rb
@@ -76,20 +76,36 @@ $bot.message do |event|
 end
 #Kick and Ban
 $bot.command :kick, min_args: 1, max_args: 1, required_permissions: [:kick_members] do |event, user|
-  event.server.kick(event.bot.parse_mention(user))
-  event.channel.send_embed do |embed|
-    embed.colour = 0xffff00
-    embed.title = "Kicked"
-    embed.description = "#{user}"
+  begin
+    event.server.kick(event.bot.parse_mention(user))
+    event.channel.send_embed do |embed|
+      embed.colour = 0xffff00
+      embed.title = "Kicked"
+      embed.description = "#{user}"
+    end
+  rescue
+    event.channel.send_embed do |embed|
+      embed.colour = 0xffff00
+      embed.title = "I can't do that for some reason"
+      embed.description = "Probably I don't have enough permissions or this user has a higher role than me"
+    end
   end
 end
 
 $bot.command :ban, min_args: 1, max_args: 1, required_permissions: [:ban_members] do |event, user|
-  event.server.ban(event.bot.parse_mention(user))
-  event.channel.send_embed do |embed|
-    embed.colour = 0xffff00
-    embed.title = "Banned"
-    embed.description = "#{user}"
+  begin
+    event.server.ban(event.bot.parse_mention(user))
+    event.channel.send_embed do |embed|
+      embed.colour = 0xffff00
+      embed.title = "Banned"
+      embed.description = "#{user}"
+    end
+  rescue
+    event.channel.send_embed do |embed|
+      embed.colour = 0xffff00
+      embed.title = "I can't do that for some reason"
+      embed.description = "Probably I don't have enough permissions or this user has a higher role than me"
+    end
   end
 end
 #Vigenere Encode and Decode
@@ -478,7 +494,7 @@ $bot.command :tanki, min_args: 1, max_args: 1 do |event, nick|
   rescue
     event.channel.send_embed do |embed|
       embed.colour = 0xffff00
-      embed.title = "I am afraid there is not such user"
+      embed.title = "I am afraid there is not such user in existance"
     end
   end
 end
