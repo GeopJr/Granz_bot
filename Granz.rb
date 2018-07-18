dd0ea1f972da5fbf165c3442b34fcc09b594cb3c
GeopJr <geopjr@outlook.com>
2018-07-19 01:08:42 +0300

    Changed domain to geopjr.xyz and added more things to --help

--- Granz.rb
+++ Granz.rb
@@ -26,7 +26,7 @@ $bot.command :help do |event|
     embed.colour = 0xffff00
     embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: "#{$bot_name}#{$bot_tag}", icon_url: "https://cdn.discordapp.com/avatars/443053627419000833/0da891379e4d9d7af31b27ec67c49a3b.png")
     embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: "\xE3\x80\x8EGeop\xE3\x80\x8F#4066", icon_url: "https://cdn.discordapp.com/avatars/216156825978929152/4726ea8789285323ca03e995b9a059bf.png")
-    embed.description = "[Command List !](https://granz.geopjr.xyz/commands.html)"
+    embed.description = "\n__[Command List !](https://granz.geopjr.xyz/commands)__\n\n__Type #{$prefix.to_s}info for some info__\n\n__Found a bug? Add me \xE3\x80\x8EGeop\xE3\x80\x8F#4066__"
   end
   rescue
       event.channel.send_embed do |embed|
