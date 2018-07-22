79576675eb03c0482bc3c4d4191d861a39de8d7e
GeopJr <geopjr@outlook.com>
2018-07-22 20:42:31 +0300

    Added waifu and husbando rating

--- Granz.rb
+++ Granz.rb
@@ -409,6 +409,40 @@ $bot.command :howcute, min_args: 1 do |event, *rating|
     end
   end
 end
+#Rates your waifu
+$bot.command :ratewaifu, min_args: 1 do |event, *rating|
+  begin
+    arr = (0..100).to_a
+    message = rating.join(" ")
+    event.channel.send_embed do |embed|
+      embed.title = "Waifu O' Meter"
+      embed.colour = 0xffff00
+      embed.description = "My rating for **#{message}** is **#{arr.sample}/100** :princess:"
+    end
+  rescue
+    event.channel.send_embed do |embed|
+      embed.colour = 0xffff00
+      embed.title = "Error"
+    end
+  end
+end
+#Rates your husbando
+$bot.command :ratehusbando, min_args: 1 do |event, *rating|
+  begin
+    arr = (0..100).to_a
+    message = rating.join(" ")
+    event.channel.send_embed do |embed|
+      embed.title = "Husbando O' Meter"
+      embed.colour = 0xffff00
+      embed.description = "My rating for **#{message}** is **#{arr.sample}/100** :prince:"
+    end
+  rescue
+    event.channel.send_embed do |embed|
+      embed.colour = 0xffff00
+      embed.title = "Error"
+    end
+  end
+end
 # Reverses something
 $bot.command :reverse, min_args: 1 do |event, *args|
   begin
