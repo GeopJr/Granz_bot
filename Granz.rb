646e4b7e21b487b4d48a6cf30c26a629b29c61c7
GeopJr <geopjr@outlook.com>
2018-07-26 17:49:28 +0300

    Added minecraft achievement generator ⛏️

--- Granz.rb
+++ Granz.rb
@@ -706,6 +706,26 @@ $bot.command :cookie do |event|
     end
   end
 end
+# Minecraft Achievement Generator
+$bot.command :achievement, min_args: 3 do |event, item, *argss|
+  begin
+    break unless item.match(/^[0-9_\-+ ]*$/)
+    args = argss.join(" ").split("|")
+    title = args[0].gsub("+", "%2B").gsub(" ", "+")
+    desc = args[1].gsub("+", "%2B").gsub(" ", "+")
+    event.channel.send_embed do |embed|
+      embed.colour = 0xffff00
+      embed.title = "Achievement Unlocked"
+      embed.image = Discordrb::Webhooks::EmbedImage.new(url: "https://www.minecraftskinstealer.com/achievement/a.php?i=#{item}&h=#{title}&t=#{desc}")
+    end
+  rescue
+    event.channel.send_embed do |embed|
+      embed.colour = 0xffff00
+      embed.title = "Error"
+      embed.description = "**#{$prefix}achievement <id> <title>|<description>**\n\nThe id's are located [__here__](https://granz.geopjr.xyz/ids.html)"
+    end
+  end
+end
 #Amiibo search
 $bot.command :amiibo, min_args: 1, max_args: 1 do |event, name|
   begin
