394a7148d02ae85f54b2e4bb2314d25e869c6862
GeopJr <geopjr@outlook.com>
2018-07-28 18:40:51 +0300

    Added --neko and embeded the sfw response

--- Granz.rb
+++ Granz.rb
@@ -652,15 +652,40 @@ $bot.command :bird do |event|
     end
   end
 end
-# #Responds with a bara image
-# $bot.command :bara do |event|
-# next event.respond "I'm sorry. I can't do that because this is a SFW channel." unless event.channel.nsfw
-#   event.channel.send_embed do |embed|
-#   embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: "https://barapi.geopjr.xyz/")
-#   embed.image = Discordrb::Webhooks::EmbedImage.new(url: HTTP.get('https://barapi.000webhostapp.com/api/random.php').parse[0])
-#   embed.color = 0xffff00
-#   end
-# end
+=begin
+# Responds with a random bara image
+$bot.command :bara do |event|
+  next event.channel.send_embed do |embed|; embed.title = "I'm sorry. I can't do that because this is a SFW channel."; embed.color = 0xffff00; embed.image = Discordrb::Webhooks::EmbedImage.new(url: "https://i.imgur.com/yVs6TqV.gif"); end unless event.channel.nsfw
+  begin
+    break if event.user.id == 446364321681768459
+    event.channel.send_embed do |embed|
+      embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: "https://barapi.geopjr.de/")
+      embed.image = Discordrb::Webhooks::EmbedImage.new(url: HTTP.get("https://barapi.000webhostapp.com/api/random.php").parse[0])
+      embed.color = 0xffff00
+    end
+  rescue
+    event.channel.send_embed do |embed|
+      embed.colour = 0xffff00
+      embed.title = "Error"
+    end
+  end
+end
+# Responds with a random lewd neko image
+$bot.command :neko do |event|
+  next event.channel.send_embed do |embed|; embed.title = "I'm sorry. I can't do that because this is a SFW channel."; embed.color = 0xffff00; embed.image = Discordrb::Webhooks::EmbedImage.new(url: "https://i.imgur.com/yVs6TqV.gif"); end unless event.channel.nsfw
+  begin
+    event.channel.send_embed do |embed|
+      embed.image = Discordrb::Webhooks::EmbedImage.new(url: HTTP.get("https://nekos.life/api/lewd/neko").parse["neko"])
+      embed.color = 0xffff00
+    end
+  rescue
+    event.channel.send_embed do |embed|
+      embed.colour = 0xffff00
+      embed.title = "Error"
+    end
+  end
+end
+=end
 #Responds with a random coffee image
 $bot.command :coffee do |event|
   begin
