0a761893b2d1d1ae29f0d4650779aba0bb0711c5
GeopJr <geopjr@outlook.com>
2018-07-30 18:17:49 +0300

    Added windows specific dirs (commented out)

--- lib/Granz.rb
+++ lib/Granz.rb
@@ -10,14 +10,19 @@ require "yaml"
 ENV["DISCORDRB_NONACL"] = "libsodium is useless here"
 # Bot module
 module Granz
-  CONFIG = YAML.load_file("../config.yaml")
+  CONFIG = YAML.load_file("config.yaml")
+  #For WINDOWS
+  #CONFIG = YAML.load_file("../config.yaml")
+  
   # Bot connect
   $bot = Discordrb::Commands::CommandBot.new token: CONFIG["$token"], client_id: CONFIG["$client_id"], prefix: CONFIG["$prefix"], ignore_bots: true, help_command: false
   # Loads all commands and events
   def self.load_modules(klass, path)
     new_module = Module.new
     const_set(klass.to_sym, new_module)
-    Dir["modules/#{path}/*.rb"].each { |file| load file }
+    Dir["#{File.dirname(__FILE__)}/modules/#{path}/*.rb"].each { |file| load file }
+    #For WINDOWS
+    #Dir["modules/#{path}/*.rb"].each { |file| load file }
     new_module.constants.each do |mod|
       $bot.include! new_module.const_get(mod)
     end
